6cf592e961e13b26d869e153df30ae538d08f134
NuralySol <soltonbekov@gmail.com>
2024-09-10 12:10:47 -0400

    second commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    
+    # new code below and you can create def's to see them print out in the python shell
+    def __str__(self):
+        return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,9 @@
-
 from django.urls import path
-# import Home view from the views file
-from .views import Home
+from .views import Home, CatList, CatDetail # additional imports
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    # new routes below 
+    path('cats/', CatList.as_view(), name='cat-list'),
+    path('cats/<int:id>/', CatDetail.as_view(), name='cat-detail'),
 ]
