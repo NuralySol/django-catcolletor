616f39a684e230701558b2d44d8cc71b72c4ccdc
NuralySol <soltonbekov@gmail.com>
2024-09-12 16:03:53 -0400

    fixes

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 # Create your views here. View is like controllers in Express JS.
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics, status, permissions # modify these imports to match
+from rest_framework import generics, status, permissions
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.exceptions import PermissionDenied
 
@@ -61,6 +61,51 @@ class CatDetail(generics.RetrieveUpdateDestroyAPIView):
         if instance.user != self.request.user:
             raise PermissionDenied({"message": "You do not have permission to delete this cat."})
         instance.delete()
+
+# Feeding views
+class FeedingListCreate(generics.ListCreateAPIView):
+    serializer_class = FeedingSerializer
+
+    def get_queryset(self):
+        cat_id = self.kwargs['cat_id']
+        return Feeding.objects.filter(cat_id=cat_id)
+
+    def perform_create(self, serializer):
+        cat_id = self.kwargs['cat_id']
+        cat = Cat.objects.get(id=cat_id)
+        serializer.save(cat=cat)
+
+class FeedingDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = FeedingSerializer
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        cat_id = self.kwargs['cat_id']
+        return Feeding.objects.filter(cat_id=cat_id)
+
+# Toy views
+class ToyList(generics.ListCreateAPIView):
+    queryset = Toy.objects.all()
+    serializer_class = ToySerializer
+
+class ToyDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Toy.objects.all()
+    serializer_class = ToySerializer
+    lookup_field = 'id'
+
+class AddToyToCat(APIView):
+    def post(self, request, cat_id, toy_id):
+        cat = Cat.objects.get(id=cat_id)
+        toy = Toy.objects.get(id=toy_id)
+        cat.toys.add(toy)
+        return Response({'message': f'Toy {toy.name} added to Cat {cat.name}'})
+
+class RemoveToyFromCat(APIView):
+    def post(self, request, cat_id, toy_id):
+        cat = Cat.objects.get(id=cat_id)
+        toy = Toy.objects.get(id=toy_id)
+        cat.toys.remove(toy)
+        return Response({'message': f'Toy {toy.name} removed from Cat {cat.name}'})
 
 # User Registration
 class CreateUserView(generics.CreateAPIView):
